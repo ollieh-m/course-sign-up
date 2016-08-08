fccaf26f2d64a6542d33aa32b8cc6a18a23ea501
Ollie Haydon-Mulligan <ollie.hm@cantab.net>
2016-08-08 10:06:34 +0000

    Validate email format for attendee

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,6 +1,7 @@
 class Attendee < ActiveRecord::Base
   
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX, message: 'You must enter a valid email' }
   validates :date_of_birth, presence: true
     
   has_many :rsvps
